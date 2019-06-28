ba4358ca40bdee81d653dd3e50236909a5b28278
Jamie Kyle <me@thejameskyle.com>
2019-06-28 13:03:43 -0700

    rebuild cargo on version

--- scripts/version.sh
+++ scripts/version.sh
@@ -3,4 +3,5 @@ set -e
 
 # shellcheck disable=SC2154
 sed -i.bak "s/version = \".*\" # generated/version = \"$npm_package_version\" \# generated/" Cargo.toml
-git add Cargo.toml
+cargo build
+git add Cargo.toml Cargo.lock
